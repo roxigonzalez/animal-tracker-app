ad0368213b4cf4565742d99a50de9d0f63720008
Roxy <roxigo@hotmail.com>
2023-05-22 18:48:18 -0600

    Added Model :card_file_box: and data dummy

---
+++ app/src/main/java/com/rgonzalez/bearapp/data/DataDummy.kt
@@ -0,0 +1,10 @@
+package com.rgonzalez.bearapp.data
+
+import com.rgonzalez.bearapp.data.model.BearModel
+
+var bears = mutableListOf<BearModel>(
+    BearModel("Yogui", "Brown", 10, 2.90),
+    BearModel("Winnie Pooh", "Yellow", 10, 1.45),
+    BearModel("Baloo", "Gray", 10, 2.81),
+    BearModel("Kenai", "Brown", 10, 1.20)
+)
---
+++ app/src/main/java/com/rgonzalez/bearapp/data/model/BearModel.kt
@@ -0,0 +1,8 @@
+package com.rgonzalez.bearapp.data.model
+
+class BearModel(
+    name: String,
+    color: String,
+    age: Int,
+    height: Double
+)
