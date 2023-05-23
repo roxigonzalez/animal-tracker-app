8eb6e67a019528a904f6e28d3970edd96345b73b
Roxy <roxigo@hotmail.com>
2023-05-22 18:55:31 -0600

    Repository of Bear :necktie:

---
+++ app/src/main/java/com/rgonzalez/bearapp/data/repositories/BearRepository.kt
@@ -0,0 +1,12 @@
+package com.rgonzalez.bearapp.data.repositories
+
+import com.rgonzalez.bearapp.data.model.BearModel
+
+class BearRepository(private val bears: MutableList<BearModel>) {
+
+    // get all bears
+    fun getBears() = bears
+
+    // function to add new bear
+    fun addBear(bear: BearModel) = bears.add(0, bear)
+}
